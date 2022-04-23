8566b60540e2af3638bfd9ec55e2873da53bcd03
keru <keruk355@gmail.com>
2022-04-23 18:38:40 +0800

    final move

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 
-const dburl = 'mongodb+srv://kerutman:bXqvqNY6sL5fNdAz@bigway.dqi7m.mongodb.net/product?retryWrites=true&w=majority'
+const dburl = process.env.PROD ? 
+  'mongodb+srv://kerutman:bXqvqNY6sL5fNdAz@bigway.dqi7m.mongodb.net/product?retryWrites=true&w=majority' : 
+  'mongodb://127.0.0.1:27017'
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,25 @@ import {Logger} from '@nestjs/common';
 const logger = new Logger('Product 🛒');
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+  const isprod = process.env.PROD ? await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
     transport: Transport.RMQ,
       options: {
-        urls: ['amqps://eoayjqkg:pHq77ZZcEV1f08J67xO1HHNGfSGTcCbB@shrimp.rmq.cloudamqp.com/eoayjqkg'],
+        urls: [process.env.RABBIT_MQ_URL || 'amqps://eoayjqkg:pHq77ZZcEV1f08J67xO1HHNGfSGTcCbB@shrimp.rmq.cloudamqp.com/eoayjqkg'],
         queue: 'product_queue',
         queueOptions: {
           durable: false
         },
       },
+  }) : 
+  await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+    transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 8000
+      },
   });
+
+  const app = isprod
   app.listen();
   logger.log('Product service is listening...');
 }
